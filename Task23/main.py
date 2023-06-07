e522e722ecb5832ad3778b3aea660e144ab521d4
BaLTaZaR <gricaev_sergey@mail.ru>
2023-06-07 14:47:03 +0600

    доработал 23 задачу

--- Task23/main.py
+++ Task23/main.py
@@ -1,6 +1,11 @@
-n = list(map(int, input().split())) # list compre
-count = 0
-for i in range(len(n) - 1):
-    if n[i] < n[i+1]:
-        count += 1
-print(count)
+n = list(map(int, input().split())) 
+# count = 0
+# for i in range(len(n) - 1):
+#     if n[i] < n[i+1]:
+#         count += 1
+# print(count)
+
+
+# Второе решение
+
+print(sum(n[i] > n[i - 1] for i in range(1, len(n))))
