5a03ca03a07d414329c45e50ba0b13930f31cb00
BaLTaZaR <gricaev_sergey@mail.ru>
2023-06-08 09:39:20 +0600

    решил 16 задачу(дз)

--- Task16/main.py
+++ Task16/main.py
@@ -1,2 +1,11 @@
 n = list(map(int, input().split()))
 x = int(input())
+count = 0
+for i in n:
+    if i == x:
+        count += 1
+print(count)
+
+# Второй способ
+
+print(sum(i == x for i in n)) #  list comprehensions
