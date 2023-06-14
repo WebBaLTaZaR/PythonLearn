3e6eca544acf43857fea53050cc96b9771fec532
BaLTaZaR <gricaev_sergey@mail.ru>
2023-06-14 14:08:24 +0600

    доработал 31 задачу

--- Task31/main.py
+++ Task31/main.py
@@ -1,10 +1,6 @@
 def fibon(n):
-    if n in [1,2]:
+    if n in [0,1]:
         return 1
     return fibon(n-1) + fibon(n-2)
 n = int(input())
-
-listFib = []
-for i in range(1, n):
-    listFib.append(fibon(i))
-print(listFib)
+print(fibon(n))
