a7879259c718c00ad6f738a28bb019ea1dac3a05
BaLTaZaR <gricaev_sergey@mail.ru>
2023-06-15 09:40:55 +0600

    дорешал 35 задачу

--- Task35/main.py
+++ Task35/main.py
@@ -1,7 +1,14 @@
 n = int(input())
+k = 0
 if n in [2,3]:
     print("Число простое")
 elif n == 1 or n % 2 == 0:
     print("Число не является простым")
 else:
-    print("Число простое")
+    for i in range(3, (n // 2) + 1,2):
+        if n % i == 0:
+            k += 1
+    if k == 0:
+        print("Число простое")
+    else:
+        print("Число не является простым")
